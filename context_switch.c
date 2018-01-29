3800c4dbb6eaad222be3f1f71966526ac2a4760a
TylerMatthews <Tyler.Matthews@me.com>
2018-01-29 13:03:24 -0500

    Add files via upload

---
+++ context_switch.c
@@ -0,0 +1,85 @@
+//**********************************************************************
+// Authors: Tyler Matthews, Matt Monik 
+// Date: 01/26/2018
+// Program to calculate the time for a System call and context switch
+//
+//
+//**********************************************************************
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <sys/time.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <pthread.h>
+#include <sched.h>
+
+//Return system time
+double calctime()
+{
+	double timeinsec = 0.0;
+	struct value ctime;
+	gettimeofday(&ctime, (struct timezone *)0);
+	timeinsec = (double)(ctime.tv_sec + ctime.tv_usec * 1.0e-3);
+
+	return(timeinsec);
+}
+
+int main(int argc, const char * argv[])
+{
+    
+    double beginning ;
+    double end;
+
+    //Set thread to one processor
+    cpu_set_t  affin;
+    CPU_ZERO(&affin);
+    unsigned int length = sizeof(affin);
+    CPU_SET(0, &affin);
+    
+
+    int     pipes[2];
+    int     byte_l;
+    char    pipeString[] = "Pipe message!\n";
+    pid_t   childId;
+    char    read_buffer[600];
+    
+    printf("Begin piping: \n");
+    
+    pipe(pipes);
+    if((childId = fork()) == -1)
+    {       
+        perror("fork");
+        exit(1);
+    }
+    
+    beginning = calctime();
+   
+    if ( (sched_setaffinity(0, length, &affin) ) < 1)
+    {
+        if(childId == 0)       
+        {       
+            //pipe input closed
+            close(pipes[0]);
+            //output to pip
+            write(pipes[1], pipe_String, (strlen(pipe_String)+1));
+            exit(0);
+        }
+    }
+    else
+    {
+		//Parent reads pip input
+        byte_l = read(pipes[0], read_buffer, sizeof(read_buffer));
+        end = calctime();
+        printf("Pipe message: %s", read_buffer);
+		printf("Context switch time: %f \n", end - beginning);
+        close(pipes[1]);
+        
+    }
+    
+
+    return(0);
+    
+}
+
---
+++ system_call.c
@@ -0,0 +1,75 @@
+//**********************************************************************
+// Authors: Tyler Matthews, Matt Monik 
+// Date: 01/26/2018
+// Program to calculate the time for a System call and context switch
+//
+//
+//**********************************************************************
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <sys/time.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <pthread.h>
+#include <sched.h>
+
+//Return system time
+double calctime()
+{
+	double timeinsec = 0.0;
+	struct value ctime;
+	gettimeofday(&ctime, (struct timezone *)0);
+	timeinsec = (double)(ctime.tv_sec + ctime.tv_usec * 1.0e-3);
+
+	return(timeinsec);
+}
+
+int main(int argc, const char * argv[])
+{
+    
+    double beginning ;
+    double end;
+
+    //Set thread to one processor
+    cpu_set_t  affin;
+    CPU_ZERO(&affin);
+    unsigned int length = sizeof(affin);
+    CPU_SET(0, &affin);
+    
+
+    int     pipes[2];
+    int     byte_l;
+    char    pipeString[] = "Pipe message!\n";
+    pid_t   childId;
+    char    read_buffer[600];
+    
+	double sysStart, sysStop, sysCallDur, dur, funcCallDur;
+
+	int i,p;
+    long k;
+    int n;
+    printf("Start Running \n");
+    long itr_a = 1000000;
+    
+    //calculate system call cost
+    sysStart = calctime();
+	for (k = 0; k < itr_a; k++)
+    {
+		i = getpid();
+	}
+    //record the time in which the system call operations ends
+	sysStop = calctime();
+
+    //systaem call time
+    dur = sysStop - sysStart;
+
+    //estimated system call time
+	sysCallDur = (sysStop - sysStart) / itr_a;
+	printf("Average system call time is %f\n", sysCallDur);
+    
+    return(0);
+    
+}
+
