c3ba2fa8526fcd30215d5f176ecfaf39587ec94b
TylerMatthews <Tyler.Matthews@me.com>
2018-01-29 14:32:01 -0500

    Update context_switch.c

--- context_switch.c
+++ context_switch.c
@@ -1,10 +1,10 @@
 //**********************************************************************
 // Authors: Tyler Matthews, Matt Monik 
 // Date: 01/26/2018
-// Program to calculate the time for a Context Switch
-//
+// Program to calculate the time for a context switch
 //
 //**********************************************************************
+#define _GNU_SOURCE
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -14,12 +14,13 @@
 #include <sys/types.h>
 #include <pthread.h>
 #include <sched.h>
+#include <time.h>
 
 //Return system time
 double calctime()
 {
 	double timeinsec = 0.0;
-	struct value ctime;
+	struct timeval ctime;
 	gettimeofday(&ctime, (struct timezone *)0);
 	timeinsec = (double)(ctime.tv_sec + ctime.tv_usec * 1.0e-3);
 
@@ -33,7 +34,7 @@ int main(int argc, const char * argv[])
     double end;
 
     //Set thread to one processor
-    cpu_set_t  affin;
+    cpu_set_t affin;
     CPU_ZERO(&affin);
     unsigned int length = sizeof(affin);
     CPU_SET(0, &affin);
@@ -53,27 +54,29 @@ int main(int argc, const char * argv[])
         perror("fork");
         exit(1);
     }
+    
     beginning = calctime();
    
-    if ( (sched_setaffinity(0, length, &affin) ) < 1)
+    if ((sched_setaffinity(0, length, &affin) ) < 1)
     {
         if(childId == 0)       
         {       
             //pipe input closed
             close(pipes[0]);
-            //output to pipe
+            //output to pip
             write(pipes[1], pipeString, (strlen(pipeString)+1));
             exit(0);
         }
-    }
-    else
-    {
-	//Parent reads pipe input
-        byte_l = read(pipes[0], read_buffer, sizeof(read_buffer));
-        end = calctime();
-        printf("Pipe message: %s", read_buffer);
-		printf("Context switch time: %f \n", end - beginning);
-        close(pipes[1]);
+        else
+        {
+    		//Parent reads pip input
+            byte_l = read(pipes[0], read_buffer, sizeof(read_buffer));
+            end = calctime();
+            printf("Pipe message: %s", read_buffer);
+    		printf("Context switch time: %10f \n", end - beginning);
+            close(pipes[1]);
+            
+        }
     }
     return(0);
     
